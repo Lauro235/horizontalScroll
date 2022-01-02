f831efabb2e0ec8e19f9be64fbdf7befefb740c8
Lorentz Bloom <lorentzbloomguitar@gmail.com>
2022-01-02 16:51:15 +0000

    little edits

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ let browserWindowX = window.innerWidth;
 const container = document.querySelector('.wrapper');
 const items = document.querySelectorAll('.item')
 
-function findWindowSize() {
+function assignWindowSize() {
     container.style.width = window.innerWidth + 'px';
     container.style.height = window.innerHeight + 'px';
     items.forEach(e => e.style.width = window.innerWidth + 'px');
     items.forEach(e => e.style.height = window.innerHeight + 'px');
 }
 
-window.onresize = findWindowSize;
+window.onresize = assignWindowSize;
