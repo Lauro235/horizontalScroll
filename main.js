189ea2a0eca02ec8049663f9a946a692e19de17d
Lorentz Bloom <lorentzbloomguitar@gmail.com>
2022-07-10 11:49:20 +0100

    method 1

--- main.js
+++ main.js
@@ -1,17 +1,80 @@
 // x = horizontal
 // y = vertical
 
-let browserWindowY = window.innerHeight;
-let browserWindowX = window.innerWidth;
+const wrapper = document.querySelector(".wrapper");
+const wrapperWidth = wrapper.scrollWidth;
+console.log(wrapperWidth);
+const item = document.querySelector(".num1");
+const itemWidth = item.scrollWidth;
+console.log(wrapperWidth);
 
-const container = document.querySelector('.wrapper');
-const items = document.querySelectorAll('.item')
+window.onload = function () {
+  window.onscroll = function () {
+    var doc = document.body,
+      scrollPosition = doc.scrollLeft;
+    console.log(scrollPosition);
+    //   pageSize = doc.scrollHeight - doc.clientHeight,
+    //   percentageScrolled = Math.floor((scrollPosition / pageSize) * 100);
+    let leftScroll;
+    for (
+      scrollPosition = 0;
+      scrollPosition < wrapper.scrollWidth;
+      scrollPosition++
+    ) {
+      leftScroll = window.scrollTo(0, scrollPosition);
+    }
+    // if (percentageScrolled == 0) {
+    //   // if the percentage is >= 50, scroll to top
+    // }
+  };
+};
 
-function assignWindowSize() {
-    container.style.width = window.innerWidth + 'px';
-    container.style.height = window.innerHeight + 'px';
-    items.forEach(e => e.style.width = window.innerWidth + 'px');
-    items.forEach(e => e.style.height = window.innerHeight + 'px');
-}
+// const transformScroll = (e) => {
+//   if(e.deltaY) {
+//     console.log(e.deltaY, 'you have scrolled down');
+//     return;
+//   }
+//   let leftScroll = e.scrollLeft += e.deltaY;
+// }
 
-window.onresize = assignWindowSize;
+// let element = document.scrollingElement || document.documentElement;
+// element.addEventListener('wheel', transformScroll)
+
+// let browserWindowY = window.innerHeight;
+// let browserWindowX = window.innerWidth;
+
+// const container = document.querySelector('.wrapper');
+// const items = document.querySelectorAll('.item')
+// let childWidth = items[0].clientWidth;
+
+// function assignWindowSize() {
+//     container.style.width = window.innerWidth + 'px';
+//     container.style.height = window.innerHeight + 'px';
+//     items.forEach(e => e.style.width = window.innerWidth + 'px');
+//     items.forEach(e => e.style.height = window.innerHeight + 'px');
+// }
+
+// console.log(childWidth);
+
+// window.onresize = assignWindowSize;
+
+// // Button to next/last picture
+
+// // button selector
+// let nextPictureButton = document.querySelectorAll('.next-picture');
+// let lastPictureButton = document.querySelectorAll('.last-picture');
+// // starting horizontal scroll coordinate
+// let scrollPoint = 0;
+
+// nextPictureButton.forEach(e => e.addEventListener('click', nextPic));
+// lastPictureButton.forEach(e => e.addEventListener('click', lastPic));
+
+// console.log(scrollPoint)
+
+// function nextPic() {
+//     container.scrollLeft = scrollPoint += childWidth;
+//     }
+
+// function lastPic() {
+//     container.scrollLeft = scrollPoint -= childWidth;
+// }
